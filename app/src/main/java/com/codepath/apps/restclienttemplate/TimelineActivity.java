f9865471e7469ee9baa0e9a7b25939995b6a2efd
Anthony Djoewanda <16738612+tonychuaco@users.noreply.github.com>
2021-10-08 23:15:22 -0400

    User can compose a new tweet, which is updated on the timeline activity.

--- app/src/main/java/com/codepath/apps/restclienttemplate/TimelineActivity.java
+++ app/src/main/java/com/codepath/apps/restclienttemplate/TimelineActivity.java
@@ -1,21 +1,25 @@
 package com.codepath.apps.restclienttemplate;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 import com.codepath.apps.restclienttemplate.models.Tweet;
 import com.codepath.asynchttpclient.callback.JsonHttpResponseHandler;
 
 import org.json.JSONArray;
 import org.json.JSONException;
+import org.parceler.Parcels;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -25,6 +29,8 @@ import okhttp3.Headers;
 public class TimelineActivity extends AppCompatActivity {
 
   public static final String TAG = "TimelineActivity";
+  private final int REQUEST_CODE = 20;
+
   TwitterClient client;
   RecyclerView rvTweets;
   List<Tweet> tweets;
@@ -60,9 +66,22 @@ public class TimelineActivity extends AppCompatActivity {
   @Override
   public boolean onOptionsItemSelected(@NonNull MenuItem item) {
     if (item.getItemId() == R.id.compose) {
-
+      Intent intent = new Intent(this, ComposeActivity.class);
+      startActivityForResult(intent, REQUEST_CODE);
+      return true;
     }
     return super.onOptionsItemSelected(item);
+  }
+
+  @Override
+  protected void onActivityResult(int requestCode, int resultCode, @Nullable @org.jetbrains.annotations.Nullable Intent data) {
+    if (requestCode == REQUEST_CODE && resultCode == RESULT_OK) {
+      Tweet tweet = Parcels.unwrap(data.getParcelableExtra("tweet"));
+      tweets.add(0, tweet);
+      adapter.notifyItemInserted(0);
+      rvTweets.smoothScrollToPosition(0);
+    }
+    super.onActivityResult(requestCode, resultCode, data);
   }
 
   @Override
